d2bd724fc74c2fad40d0bce5761399b5be1513d0
pingu543 <pcu50208@outlook.com>
2026-09-03 17:18:17 -0400

    Updated resources and connect with me sections.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,11 @@ function Header() {
     <header id="top-bar">
       <Hero />
       <div>
-        <h1>Get started</h1>
+        {/* <h1>Get started</h1> */}
+        <h1>Ping Ueng</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          {/* Edit <code>src/App.tsx</code> and save to test <code>HMR</code> */}
+          Software Engineer | Full Stack Developer
         </p>
       </div>
       <ClickCounter />
---
+++ src/components/Resources.tsx
@@ -0,0 +1,65 @@
+import reactLogo from '../assets/react.svg'
+import viteLogo from '../assets/vite.svg'
+
+function Resources() {
+  return (
+    <section id="resources">
+      <div id="docs">
+        <svg className="icon" role="presentation" aria-hidden="true">
+          <use href="/icons.svg#documentation-icon"></use>
+        </svg>
+        <h2>Vite & React documentation</h2>
+        <p>The official documentation for Vite and React</p>
+        <ul>
+          <li>
+            <a href="https://vite.dev/" target="_blank">
+              <img className="logo" src={viteLogo} alt="" />
+              Explore Vite
+            </a>
+          </li>
+          <li>
+            <a href="https://react.dev/" target="_blank">
+              <img className="button-icon" src={reactLogo} alt="" />
+              Learn more
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div id="social">
+        <svg className="icon" role="presentation" aria-hidden="true">
+          <use href="/icons.svg#social-icon"></use>
+        </svg>
+        <h2>Connect with me</h2>
+        <p>My social media</p>
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/ping-chang-ueng/" target="_blank">
+              <svg
+                className="button-icon"
+                role="presentation"
+                aria-hidden="true"
+              >
+                <use href="/icons.svg#linkedin-icon"></use>
+              </svg>
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/pingu543" target="_blank">
+              <svg
+                className="button-icon"
+                role="presentation"
+                aria-hidden="true"
+              >
+                <use href="/icons.svg#github-icon"></use>
+              </svg>
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Resources
